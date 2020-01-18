0deced33635362e3256820a5d7718ae0f2e3af84
Omid Mogasemi <omidmogasemi@gmail.com>
2020-01-18 14:43:03 -0800

    Added Firebase System

--- app/src/main/java/com/hackdavis/komma/MainActivity.java
+++ app/src/main/java/com/hackdavis/komma/MainActivity.java
@@ -2,13 +2,37 @@ package com.hackdavis.komma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeFields();
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        if (currentUser == null)
+            sendUserToLoginActivityWithFlags();
+    }
+
+    private void sendUserToLoginActivityWithFlags() {
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(loginIntent);
+        finish();
+    }
+
+    private void initializeFields() {
+
     }
 }
