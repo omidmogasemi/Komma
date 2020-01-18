e32790348d356e81f118fc24a1b6a8a336ecef33
akchakravarthy <akchakravarthy@ucdavis.edu>
2020-01-18 14:38:45 -0800

    created new file in /res/xml/

---
+++ app/src/main/java/com/hackdavis/komma/SearchActivity.java
@@ -0,0 +1,24 @@
+package com.hackdavis.komma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        Intent intent = getIntent();
+        if(Intent.ACTION_SEARCH.equals(intent.getAction())){
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doSearch(query);
+        }
+    }
+    public static String doSearch(String query){
+
+    }
+}
