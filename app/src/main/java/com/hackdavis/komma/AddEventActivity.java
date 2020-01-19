4cb4f2da7bbc9ccab57ec6ddabb8b2f074d398a3
akchakravarthy <akchakravarthy@ucdavis.edu>
2020-01-18 16:45:30 -0800

    added datepickerfragment

--- app/src/main/java/com/hackdavis/komma/AddEventActivity.java
+++ app/src/main/java/com/hackdavis/komma/AddEventActivity.java
@@ -1,8 +1,18 @@
 package com.hackdavis.komma;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class AddEventActivity extends AppCompatActivity {
 
@@ -10,5 +20,34 @@ public class AddEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+    }
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        public DatePickerFragment(){}
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            int month = c.get(Calendar.MONTH);
+            int year = c.get(Calendar.YEAR);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, day, month, year);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the time chosen by the user
+        }
+    }
+    public void showDatePickerDialogStartDate(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void showDatePickerDialogEndDate(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
