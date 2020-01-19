9124fcd5269126a52a3c0ece20538f9561c0d59b
Essam Sleiman <essamfsleiman@gmail.com>
2020-01-19 01:08:48 -0800

    Fixed the UI

--- app/src/main/java/com/hackdavis/komma/FragmentActivity.java
+++ app/src/main/java/com/hackdavis/komma/FragmentActivity.java
@@ -3,12 +3,52 @@ package com.hackdavis.komma;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class FragmentActivity extends AppCompatActivity {
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FragmentActivity extends Fragment {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_fragment,
+                container, false);
+        return view;
+    }
+
+
+
+    public void setNameText(String text) {
+        TextView view = (TextView) getView().findViewById(R.id.name_View);
+        view.setText(text);
+    }
+
+    public void setLocationText(String text) {
+        TextView view = (TextView) getView().findViewById(R.id.location_View);
+        view.setText(text);
+    }
+
+    public void setDescriptionText(String text) {
+        TextView view = (TextView) getView().findViewById(R.id.description_View);
+        view.setText(text);
+    }
+
+    public void numberGoingText(String text) {
+        TextView view = (TextView) getView().findViewById(R.id.numberGoing_View);
+        view.setText(text);
+    }
+
+    public void dateTimeText(String text) {
+        TextView view = (TextView) getView().findViewById(R.id.dateTime_View);
+        view.setText(text);
     }
 }
